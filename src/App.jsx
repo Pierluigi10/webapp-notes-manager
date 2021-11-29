a0f4bc38b8972d0b17124f45a5c00493418c7c2c
pierluigi <pierluigibaiano@gmail.com>
2021-11-29 18:07:00 +0100

    create a context

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { NotesContext } from "./contex";
 import { CreateNoteScreen } from "./CreateNoteScreen";
 import { HomeScreen } from "./HomeScreen";
 
 function App() {
+  const [notes, setNotes] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          <Link to="/"> Notes App </Link>
-        </h1>
-        <Link to="/create"> create </Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/create" element={<CreateNoteScreen />} />
-        </Routes>
-      </main>
-      <footer>footer</footer>
-    </div>
+    <NotesContext.Provider value={useState({ notes, setNotes })}>
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            <Link to="/"> Notes App </Link>
+          </h1>
+          <Link to="/create"> create </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/create" element={<CreateNoteScreen />} />
+          </Routes>
+        </main>
+        <footer>footer</footer>
+      </div>
+    </NotesContext.Provider>
   );
 }
 
---
+++ src/contex.js
@@ -0,0 +1,1 @@
+export const NotesContext = React.createContext();
