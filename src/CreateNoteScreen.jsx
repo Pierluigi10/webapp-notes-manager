b38823ae2369bc5eec7765725b564c634cb73af9
pierluigi <pierluigibaiano@gmail.com>
2021-11-29 18:53:22 +0100

    create a form to add notes

--- src/CreateNoteScreen.jsx
+++ src/CreateNoteScreen.jsx
@@ -1,7 +1,32 @@
+import React, { useContext, useState } from "react";
+import { NotesContext } from "./context";
+
 export function CreateNoteScreen() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const { notes, setNotes } = useContext(NotesContext);
   return (
     <>
       Create Note
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button
+        onClick={(e) => {
+          setNotes([...notes, { title, content }]);
+          setTitle("");
+          setContent("");
+        }}
+      >
+        Create Note
+      </button>
     </>
   );
 }
