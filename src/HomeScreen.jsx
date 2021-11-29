8c224edab2868079ef2e140d1b2ad11c0515edf4
pierluigi <pierluigibaiano@gmail.com>
2021-11-29 18:37:12 +0100

    define HomeScreen and his features

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,11 +1,25 @@
 import React, { useContext } from "react";
 import { NotesContext } from "./context";
 
+function Note({ note }) {
+  return (
+    <div className="note">
+      <h2>{note.title}</h2>
+      <p>{note.content}</p>
+    </div>
+  );
+}
+
 export function HomeScreen() {
   const { notes, setNotes } = useContext(NotesContext);
   return (
     <>
-      <h3>Home</h3>
+      <h3>Notes</h3>
+      <ul>
+        {notes.map((note, position) => (
+          <Note key={position} note={note} />
+        ))}
+      </ul>
       <pre>{JSON.stringify(notes)}</pre>
     </>
   );
