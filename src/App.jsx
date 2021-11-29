0d79f752a98898f7f073c4744fb4ff9d79493182
pierluigi <pierluigibaiano@gmail.com>
2021-11-29 17:49:28 +0100

    add Route and Routes and create function /component HomeScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
+function HomeScreen(){
+  return(
+    <>
+    <h3>Home</h3>
+    </>
+  )
+}
+
 
 function App() {
-
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-      </header>
+        <header className="App-header">
+          <p>Notes App</p>
+        </header>
+        <main>
+      <Routes>
+          <Route path="/" element={<HomeScreen />} />
+      </Routes>
+        </main>
+        <footer>footer</footer>
     </div>
   );
 }
