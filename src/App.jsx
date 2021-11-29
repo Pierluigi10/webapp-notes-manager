3277d0fba5975aa820f324bed3bec25a547e01dc
pierluigi <pierluigibaiano@gmail.com>
2021-11-29 20:22:22 +0100

    install and import uikit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import { CreateNoteScreen } from "./CreateNoteScreen";
 import { HomeScreen } from "./HomeScreen";
 import { EditNoteScreen } from "./EditNoteScreen";
 
+import "../node_modules/uikit/dist/css/uikit.min.css";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+//loads the icon plugin
+UIkit.use(Icons);
+
 function App() {
   const [notes, setNotes] = useState([
     { id: 1, tile: "Note 1", content: "Note 1 content" },
@@ -17,9 +24,9 @@ function App() {
 
   return (
     <NotesContext.Provider value={{ notes, setNotes, deleteNote }}>
-      <div className="App">
+      <div className="uk.container">
         <header className="App-header">
-          <h1>
+          <h1 className=".uk-text-lead">
             <Link to="/"> Notes App ({notes.length}) </Link>
           </h1>
           <Link to="/create"> create </Link>
